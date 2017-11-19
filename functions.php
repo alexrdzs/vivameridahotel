b0e23006e998761e2a2189aff91b0cc40bb4cdef
alex rodríguez santibáñez <alex@alexrds.com>
2017-11-19 20:57:31 +0100

    Hide Update Notification

--- functions.php
+++ functions.php
@@ -151,3 +151,12 @@ function thim_child_enqueue_styles()
 	}
 
 add_action('wp_enqueue_scripts', 'thim_child_enqueue_styles', 100);
+
+// Hide the "Please update now" notification
+function hide_update_notice() {
+    get_currentuserinfo();
+    if (!current_user_can('manage_options')) {
+        remove_action( 'admin_notices', 'update_nag', 3 );
+    }
+}
+add_action( 'admin_notices', 'hide_update_notice', 1 );
